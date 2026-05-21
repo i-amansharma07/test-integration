8c6efd5530e9a5e0ed58e97c12fa05bad27d93ca
Aman Sharma <aman.sharma@greyspacedigital.com>
2026-05-21 14:02:00 +0530

    Add prisma setup in script and remove from test.yml

--- scripts/run-integration.sh
+++ scripts/run-integration.sh
@@ -2,7 +2,8 @@ docker compose up -d
 echo '🟡 - Waiting for database to be ready...'
 ./wait-for-it.sh "postgresql://postgres:mysecretpassword@localhost:5432/postgres" -- echo '🟢 - Database is ready!'
 cd ..
-npx prisma migrate dev --name init
+npm run db:migrate
+npm run db:generate
 npm run test
 cd scripts
 docker compose down
